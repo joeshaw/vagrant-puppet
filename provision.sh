0fdfe35358ff6506693e32fdc231ae2ea1fff9d9
Joe Shaw <joeshaw@litl.com>
2011-10-04 16:09:19 -0400

    save vagrant configuration for testing puppet deployments

---
+++ provision.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Die on any error
+set -e
+
+# Change hostname to 'vagrant'
+oldname=$(hostname)
+hostname vagrant
+sed -i -e s/$oldname/vagrant/g /etc/hosts
+
+# Install software
+aptitude update
+aptitude install -y puppet=0.25.4-2ubuntu6 emacs23-nox git-core
+
+# bind mount /vagrant/puppet into /etc/puppet
+mv /etc/puppet /etc/puppet.old
+mkdir /etc/puppet
+mount --bind /vagrant/puppet /etc/puppet
